c0dbec066a57ab746f3942751fe4d4192ff02c30
Alifio Rasendriya Rasyid <official.alifio@gmail.com>
2023-09-30 02:27:16 +0700

    Initial project commit

---
+++ src/test/java/com/hudel/web/backend/HudelWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudel.web.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HudelWebBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
